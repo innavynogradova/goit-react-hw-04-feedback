58c871aec1ad2cefd0145c0e05f120fd6af5cff1
innavynogradova <inna.vinogradova80@gmail.com>
2022-08-29 00:52:45 +0300

    changed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { ThemeProvider } from 'theme-ui';
-import theme from './theme';
+import { theme } from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
